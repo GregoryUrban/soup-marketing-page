a6174a190bc74f938767704c4723ef8a859d71db
GregoryUrban <turknben@gmail.com>
2019-02-21 15:10:38 -0500

    Added AboutUs less file, styled about us page

--- components/carousel.js
+++ components/carousel.js
@@ -1,11 +1,11 @@
 class Carousel {
-    constructor(carousel){ //    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
+    constructor(carousel){ //    reference to the carousel with the laft and right buttons
     this.carousel = carousel;
     this.imgs = document.querySelectorAll(`.carousel img`); //    2. You will need to grab a reference to all of the images
     this.left = document.querySelector('.left-button');//    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
     this.right = document.querySelector('.right-button');//    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
 
-    this.index = 0; //    3. Create a current index
+    this.index = 0; //    current index
     this.imgs[this.index].style.display = "block";
 
     this.left.addEventListener("click", () => this.toggleLeft());
@@ -13,7 +13,7 @@ class Carousel {
     
     }
 
-    toggleLeft() { //    4. Those buttons are gonna need some click handlers.
+    toggleLeft() { //  handlers.
         this.imgs[this.index].style.display = "none";
 
         if (this.index === 0) {
@@ -24,7 +24,7 @@ class Carousel {
         this.imgs[this.index].style.display = "block";
     }
     
-    toggleRight() {//    4. Those buttons are gonna need some click handlers.
+    toggleRight() {//  handlers.
         this.imgs[this.index].style.display = "none";
 
         if (this.index ===  this.imgs.length-1) {
@@ -38,3 +38,44 @@ class Carousel {
 }
 
 const carousels = document.querySelectorAll(".carousel").forEach(carousel => new Carousel(carousel));
+
+
+// trying to auto-slide through carousel images
+// var car = document.querySelectorAll('.car');
+// for(var i=0; i<car.length; i++){
+// 	car[i].style.display = 'inline-block';
+// }
+
+// var slides = document.querySelectorAll('.car img');
+// var currentSlide = 0;
+// var slideInterval = setInterval(nextSlide,2000);
+
+// function nextSlide(){
+// 	goToSlide(currentSlide+1);
+// }
+
+// function previousSlide(){
+// 	goToSlide(currentSlide-1);
+// }
+
+// function goToSlide(n){
+// 	car[currentSlide].className = 'car';
+// 	currentSlide = (n+car.length)%car.length;
+// 	car[currentSlide].className = 'slide showing';
+// }
+
+
+// var playing = true;
+// var pauseButton = document.getElementById('pause');
+
+// function pauseSlideshow(){
+// 	pauseButton.innerHTML = '&#9658;'; // play character
+// 	playing = false;
+// 	clearInterval(slideInterval);
+// }
+
+// function playSlideshow(){
+// 	pauseButton.innerHTML = '&#10074;&#10074;'; // pause character
+// 	playing = true;
+// 	slideInterval = setInterval(nextSlide,2000);
+// }
